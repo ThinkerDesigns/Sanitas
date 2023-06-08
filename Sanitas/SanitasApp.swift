5bdf1fccbf5b8f88285d9acc3f8272c9801c5df1
ThinkerDesigns <usb163016@gmail.com>
2023-06-08 16:29:19 -0500

    Initial Commit

---
+++ Sanitas/SanitasApp.swift
@@ -0,0 +1,17 @@
+//
+//  SanitasApp.swift
+//  Sanitas
+//
+//  Created by Dhruv Bhadauriya on 6/8/23.
+//
+
+import SwiftUI
+
+@main
+struct SanitasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
